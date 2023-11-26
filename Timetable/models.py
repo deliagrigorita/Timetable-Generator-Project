fa335ce83fe44f06692136bb28ecdb3c8b3a8a9c
Raul-Madalin <raul05madalin@yahoo.com>
2023-11-26 16:48:46 +0200

    Created models from class diagram

--- Timetable/models.py
+++ Timetable/models.py
@@ -33,3 +33,50 @@ class Teacher(User):
     class Meta:
         verbose_name = 'Teacher'
         verbose_name_plural = 'Teachers'
+
+
+class Resource(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    classroom = models.ForeignKey('Classroom', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Classroom(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Schedule(models.Model):
+    id = models.AutoField(primary_key=True)
+    day = models.CharField(max_length=15)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+
+    def __str__(self):
+        return f"{self.day} - {self.start_time} to {self.end_time}"
+
+
+class Class(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    students = models.ManyToManyField(Student)
+    classroom = models.ForeignKey(Classroom, on_delete=models.CASCADE)
+    schedule = models.ForeignKey(Schedule, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Timetable(models.Model):
+    id = models.AutoField(primary_key=True)
+    classes = models.ForeignKey(Class, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Timetable {self.id}"
